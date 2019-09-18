e8be4d9607352bebaadd513ec4d6d45a6c14b7e0
Norman Huth <contact@normanhuth.com>
2019-09-18 21:14:05 +0200

    Update V 1.1.1 * Add mixed content * add animated anchor * Navbar toggler

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,3 +1,25 @@
 //=include ../../../node_modules/jquery/dist/jquery.js
 //=include ../popper.js
 //=include ../../../node_modules/bootstrap/dist/js/bootstrap.js
+
+$(document).ready(function() {
+    const back_to_top_button = ['<a href="#body" class="back-to-top anchor btn btn-sm btn-primary" id="btn_ttt"><i class="fas fa-long-arrow-alt-up fa-fw"></i></a>'].join("");
+    $("body").append(back_to_top_button);
+    $(".back-to-top").hide();
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.back-to-top').fadeIn();
+            } else {
+                $('.back-to-top').fadeOut();
+            }
+        });
+    });
+    const $root = $('html, body');
+    $('a[href^="#"].anchor').click(function () {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top-60
+        }, 800);
+        return false;
+    });
+});
